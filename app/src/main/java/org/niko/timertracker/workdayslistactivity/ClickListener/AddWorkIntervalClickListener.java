fcf1cde75d8ae40ae2af9979974ff08c76a293f4
Nikolai Riedel <alpinewebdev@gmail.com>
2019-10-21 18:21:43 +0200

    Fix bug with addWorkdayButton

--- app/src/main/java/org/niko/timertracker/workdayslistactivity/ClickListener/AddWorkIntervalClickListener.java
+++ app/src/main/java/org/niko/timertracker/workdayslistactivity/ClickListener/AddWorkIntervalClickListener.java
@@ -4,9 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.niko.timertracker.workdayslistactivity.Persistence.FirebaseService;
 import org.niko.workdayslistactivity.timertracker.R;
@@ -28,14 +27,15 @@ public class AddWorkIntervalClickListener implements View.OnClickListener {
         Intent intent = new Intent("add.workInterval.listAdapter_243985sadnfoiu2");
         intent.putExtra("workdayId", this.workdayId);
         intent.putExtra("isStartTime", this.isStartTime);
-        FloatingActionButton floatingActionButton = v.findViewById(R.id.addWorkIntervalButton);
+        AppCompatButton appCompatButton = v.findViewById(R.id.addWorkIntervalButton);
 
         if (FirebaseService.isIsWorkIntervalRunning()) {
-            floatingActionButton.setImageResource(R.drawable.ic_playicon);
+            appCompatButton.setBackgroundResource(R.drawable.ic_playicon);
+
             FirebaseService.setIsWorkIntervalRunning(false);
         }
         else {
-            floatingActionButton.setImageResource(R.drawable.ic_stopicon);
+            appCompatButton.setBackgroundResource(R.drawable.ic_stopicon);
             FirebaseService.setIsWorkIntervalRunning(true);
         }
         LocalBroadcastManager.getInstance(this.context).sendBroadcast(intent);
